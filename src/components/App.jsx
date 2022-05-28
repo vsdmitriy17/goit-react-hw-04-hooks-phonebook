581351651e82c84caa91a9d595be8f957a59a291
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2022-05-28 23:50:05 +0300

    feature/phonebook-hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,54 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './App.module.css';
-import SectionTitle from './sectionTitle/SectionTitle.jsx';
-import Feedback from './feedback/Feedback.jsx';
-import Statistics from './statistics/Statistics.jsx';
-import Notification from './notification/Notification.jsx';
+import ContactList from './contactList/ContactList.jsx';
+import ContactForm from './contactForm/ContactForm.jsx';
+import Filter from './filter/Filter.jsx';
 
 export default function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+    const [contacts, setContacts] = useState(JSON.parse(window.localStorage.getItem("contacts")) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }
+    ]);
+    const [filter, setFilter] = useState('');
 
-  const feedBackIncrement = name => {
-    switch(name) {
-      case 'good':
-        setGood(prevState => prevState + 1);
-        break;
-      
-      case 'neutral':
-        setNeutral(prevState => prevState + 1);
-        break;
-      
-      case 'bad':
-        setBad(prevState => prevState + 1);
-        break;
-      
-      default:
-        return;
+    useEffect(() => {
+        window.localStorage.setItem("contacts", JSON.stringify(contacts));
+    }, [contacts])
+
+    const formSubmit = data => {
+        setContacts(prevState => {
+            const isNameExist = prevState.find(contact => contact.name === data.name);
+            return (isNameExist ? alert(`${data.name} is already in contacts`) : [data, ...prevState]);
+        })
+    };
+
+    const deleteContact = (currentId) => {
+        setContacts(prevState => {
+            return prevState.filter(contact => contact.id !== currentId)
+        });
+    };
+
+    const changeFilter = (evt) => {
+        setFilter(evt.currentTarget.value);
     }
-  };
 
-  const countTotalFeedback = () => {
-    return (good + neutral + bad);
-  };
+    const getVisibleContacts = () => {
+        const filterNormalized = filter.toLowerCase();
+        return contacts.filter(contact => contact.name.toLowerCase().includes(filterNormalized));
+    }
 
-  const countPositiveFeedbackPercentage = () => {
-    return Math.round((good * 100 / (good + neutral + bad)));
-  };
+    const visibleContacts = getVisibleContacts();
 
-  const nameBtn = ['good', 'neutral', 'bad'];
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Phonebook</h1>
+            <ContactForm onSubmit={formSubmit}/>
 
-  return (
-    <div className={styles.container}>
-        <SectionTitle title="Please leave feedback">
-            <Feedback
-                options={nameBtn}
-                onLeaveFeedback={feedBackIncrement}
-            />
-        </SectionTitle>
-        <SectionTitle title="Statistics">
-            {(good || neutral || bad) ? (
-            <Statistics
-              pervState={{ good, neutral, bad }}
-              total={countTotalFeedback()}
-              positive={countPositiveFeedbackPercentage()}
-            />
-            ) : <Notification message="No feedback given"/>}
-        </SectionTitle>
-    </div>
-  );
+            <h2 className={styles.title}>Contacts</h2>
+            <Filter value={filter} onChangeFilter={changeFilter}/>
+            <ContactList contacts={visibleContacts} onDeleteContact={deleteContact}/>
+        </div>
+    );
 }
