b7f735262821165068785cfc24b734a980bf6274
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2022-05-29 08:51:36 +0300

    App/update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,14 @@ import ContactForm from './contactForm/ContactForm.jsx';
 import Filter from './filter/Filter.jsx';
 
 export default function App() {
-    const [contacts, setContacts] = useState(JSON.parse(window.localStorage.getItem("contacts")) ?? [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }
-    ]);
+    const [contacts, setContacts] = useState(() => {
+        return JSON.parse(window.localStorage.getItem("contacts")) ?? [
+            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }
+        ];
+    });
     const [filter, setFilter] = useState('');
 
     useEffect(() => {
